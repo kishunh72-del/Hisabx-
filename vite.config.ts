f8beaccefcf47d727f800859651945d87e848e64
kishunh72-del <kishunh72@gmail.com>
2026-09-08 20:49:34 +0530

    chore: update base URL path in vite config
    Adjust the base path to align with the deployment environment.

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/HisabX/',
+    base: '/HisabX-/',
     server: {
       hmr: process.env.DISABLE_HMR !== 'true',
       watch: process.env.DISABLE_HMR === 'true' ? null : {},
