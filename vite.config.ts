036f9ae99aadb1425db04648c4da902696fde2f1
kishunh72-del <kishunh72@gmail.com>
2026-09-09 16:22:30 +0530

    feat: integrate tailwindcss
    Add Tailwind CSS support and update base path.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import {defineConfig} from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(() => {
   return {
-    base: '/HisabX-/',
+    base: '/Hisabx-/',
+    plugins: [
+      tailwindcss(),
+    ],
     server: {
       hmr: process.env.DISABLE_HMR !== 'true',
       watch: process.env.DISABLE_HMR === 'true' ? null : {},
